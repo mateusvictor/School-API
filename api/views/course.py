b257dfff88ec94812ef1290d98323602ec010cc5
Mateus Victor <mateus_victors@outlook.com>
2021-05-30 14:59:36 -0300

    CourseDetailView

--- api/views/course.py
+++ api/views/course.py
@@ -10,5 +10,33 @@ from api.models import *
 class CourseList(APIView):
 	def get(self, request):
 		courses = Course.objects.all()
-		serializer = CourseSerializer(courses, many=True)
+		serializer = CourseSerializer(courses, many=True, context={'request': request})
 		return Response(serializer.data)
+
+
+class CourseDetail(APIView):
+	def get_object(self, pk):
+		try:
+			return Course.objects.get(pk=pk)
+		except Course.DoesNotExist:
+			raise Http404
+
+	def get(self, request, pk):
+		course = self.get_object(pk)
+		serializer = CourseSerializer(course, context={'request': request})
+		return Response(serializer.data)
+
+	def put(self, request, pk):
+		course = self.get_object(pk)
+		serializer = CourseSerializer(course, data=request.data)
+
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_200_OK)
+
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def delete(self, request, pk):
+		course = self.get_object(pk)		
+		course.delete()
+		return Response("[]", status=status.HTTP_200_OK)

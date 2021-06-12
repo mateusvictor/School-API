fdcff82a14e255969ee2283b79cf86c47c99fc09
Mateus Victor <mateus_victors@outlook.com>
2021-06-12 00:07:30 -0300

    add creation test for Professor model

--- api/tests.py
+++ api/tests.py
@@ -1,9 +1,50 @@
+from decimal import Decimal
 from django.urls import reverse
 from django.forms.models import model_to_dict
 import json
 from rest_framework import status
 from rest_framework.test import APITestCase
 from api.models import *
+
+
+class ProfessorTests(APITestCase):
+	def test_create_professor(self):
+		"""
+		Ensure we can create a new student object
+		"""
+		url = reverse('professor-list')
+		data = { 
+			'person': {
+				'first_name': 'Mateus',
+				'last_name': 'Victor',
+				'date_of_birth': '2000-11-21',
+				'email': 'victors_@yahoo.com',
+				'address': {
+					'country': 'Brazil',
+					'state': 'Sao Paulo',
+					'city': 'Sao Paulo',
+					'street': 'Avenida Brasil, 2111',
+					'postal_code': '21232-009',
+				}
+			},
+			'salary': 9213.12,
+			'entry_year': 2000
+		}
+
+		response = self.client.post(url, data, format='json')
+		response_dict = dict(response.data)
+
+		professor_object = Professor.objects.get(pk=int(response_dict['id']))
+		person_object = professor_object.person
+		address_object = person_object.address
+
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+		self.assertEqual(data['person']['first_name'], person_object.first_name)
+		self.assertEqual(data['person']['address']['street'], address_object.street)
+		self.assertEqual(Decimal(str(data['salary'])), professor_object.salary)
+
+
+
 
 
 class StudentTests(APITestCase):

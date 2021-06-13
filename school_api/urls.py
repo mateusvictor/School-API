b6f60de0b4a1db7cd9c9f4f41d03a8316be8c905
Mateus Victor <mateus_victors@outlook.com>
2021-06-13 17:04:12 -0300

    add Swagger routes

--- school_api/urls.py
+++ school_api/urls.py
@@ -1,22 +1,22 @@
-"""school_api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.conf.urls.static import static
+from school_api import settings
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+	path('openapi/', get_schema_view(
+		title="School API made using Django Rest Framework",
+		description="Here you can read, create, edit and delete professors, students, courses and more.",
+		version="1.0.0"
+	), name='openapi-schema'),
+
+	path('', TemplateView.as_view(
+		template_name='swagger-ui.html',
+		extra_context={'schema_url':'openapi-schema'}
+	), name='swagger-ui'),	
 ]
